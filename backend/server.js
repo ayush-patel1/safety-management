aa9d512770abf5c7ab1c1d7ee1f97d083a6d9d95
ayushpatel1 <ayush005rkt@gmail.com>
2025-06-02 20:56:35 +0530

    api test and fixing

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,11 @@ dotenv.config()
 const app = express()
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],  
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
