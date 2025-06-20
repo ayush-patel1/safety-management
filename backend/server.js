384d9f577ecb1ed3a38e1a46595a9e0fa2c94d25
ayushpatel1 <ayush005rkt@gmail.com>
2025-06-20 14:17:53 +0530

    error fix

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],  
 }))
